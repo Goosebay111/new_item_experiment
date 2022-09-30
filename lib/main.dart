471c0c0bf47dd6055e79e2760a8fcaef2d65decc
George Lee <george.p.lee.iii@gmail.com>
2022-09-30 14:25:44 +1000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:new_item_experiment/blocs/item_bloc.dart';
+import 'package:new_item_experiment/pages/home_page.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+  HydratedBlocOverrides.runZoned(
+    () => runApp(RepositoryProvider(
+      create: (context) => SelectedBloc(),
+      child: const MyApp(),
+    )),
+    storage: storage,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SelectedBloc(),
+      child: const MaterialApp(
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
